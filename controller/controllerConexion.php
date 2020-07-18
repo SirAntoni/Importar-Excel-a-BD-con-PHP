3d49defde57fe55cecc5a0ecf6b52f7dd9d7fce2
Antony Culqui <a.culqui02@gmail.com>
2020-07-18 10:19:42 -0500

    Se creo formulario, estilos y se agrego la conexión a la base de datos.

---
+++ controller/controllerConexion.php
@@ -0,0 +1,21 @@
+<?php
+
+function obtenerBD()
+{
+    $nombre_base_de_datos = "fedorfinal";
+    $usuario              = "root";
+    $contraseña          = "";
+    try {
+
+        $base_de_datos = new PDO('mysql:host=localhost;dbname=' . $nombre_base_de_datos, $usuario, $contraseña);
+        $base_de_datos->query("set names utf8;");
+        $base_de_datos->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        $base_de_datos->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $base_de_datos->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+        return $base_de_datos;
+    } catch (Exception $e) {
+        # Nota: ¡en la vida real no imprimas errores!
+        exit("Error obteniendo BD: " . $e->getMessage());
+        return null;
+    }
+}
---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Subir excel a Base de datos con PHP | Antoni.me</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+	<link rel="stylesheet" href="css/estilos.css">
+</head>
+<body>
+
+	<div class="container">
+		<h4 class="text-center mb-4">Importar Tablas Excel a Base de datos con PHP</h4>
+		<form action="" method="POST">
+			<div class="row justify-content-center">
+				<div class="col-md-7">
+					<div class="form-group">
+						<label for="cargar documento">Cargar documento excel:</label>
+						<div class="custom-file">
+							<input type="file" class="custom-file-input" id="customFile">
+							<label class="custom-file-label" for="customFile">Seleccionar archivo</label>
+						</div>
+					</div>
+					<button type="submit" class="btn btn-primary">Guardar</button>
+				</div>
+			</div>
+		</form>
+	</div>
+
+	<script
+	src="https://code.jquery.com/jquery-3.5.1.js"
+	integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc="
+	crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+</html>
